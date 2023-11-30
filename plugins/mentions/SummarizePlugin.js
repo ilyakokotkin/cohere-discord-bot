04ceef90e997ee2fe3c16d862abf6d1b151d37b2
ilyakokotkin <130189634+ilyakokotkin@users.noreply.github.com>
2023-11-30 05:12:54 +0000

    Update SummarizePlugin.js

--- plugins/mentions/SummarizePlugin.js
+++ plugins/mentions/SummarizePlugin.js
@@ -1,13 +1,14 @@
 const cohere = require('cohere-ai');
 cohere.init(process.env.COHERE_API_KEY);
 
+//Call Cohere API to make a summary
 const execute = async (message, content) => {
-  // Here use Cohere's API to summarize the content
   const response = await cohere.generate({
     model: 'large',
     prompt: `Summarize this: ${content}\n\nSummary:`,
     max_tokens: 50,
   });
+  
   //Send summary as a reply
   message.reply(response.body.generations[0].text);
 };
